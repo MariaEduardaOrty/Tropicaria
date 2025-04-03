6726b2d28d4be2172cae065210bfc95bd1a00b38
MariaEduardaOrty <142507594+MariaEduardaOrty@users.noreply.github.com>
2025-04-03 20:38:42 -0300

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded',function(){
     botaoDeAcessibilidade.addEventListener('click' , function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+        
         const botaoSelecionado=botaoDeAcessibilidade.getAtrribute('aria-expanded')=== 'true';
+
+        botaoDeAcessibilidade.setAttribute('aria-expanded', botaoSelecionado);
 
 
     })
-  
+    
 
 
 })
