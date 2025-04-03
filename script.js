c9df9272a7f231eb9e3c69c4b24b591ce8c95798
MariaEduardaOrty <142507594+MariaEduardaOrty@users.noreply.github.com>
2025-04-03 20:34:11 -0300

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded',function(){
     botaoDeAcessibilidade.addEventListener('click' , function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+        const botaoSelecionado=botaoDeAcessibilidade.getAtrribute('aria-expanded')=== 'true';
 
 
     })
-    
+  
 
 
 })
-const botaoSelecionado=botaoDeAcessibilidade.getAtrribute('aria-expanded')=== 'true';
+
